c72896a3227e414db3fca4611679ffcd98eb0f72
leodotng <josh@leo.ng>
2018-05-02 00:48:14 -0600

    initial setup

---
+++ server.js
@@ -0,0 +1,11 @@
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var graphql = require('graphql');
+
+var GraphQLSchema = graphql.GraphQLSchema;
+var GraphQLObjectType = graphql.GraphQLObjectType;
+var GraphQLString = graphql.GraphQLString;
+var GraphQLInt = graphql.GrapQLInt;
+
